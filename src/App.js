e250efe608ac0ad731a427a127c44be0698cef98
uRaczy <raczy7@gmail.com>
2020-09-27 19:00:17 +0200

    added shop apge, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-  );
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return(
@@ -16,13 +11,8 @@ function App() {
         {/* 
         for router without "exact" attribute every route is basically a "/" path, because every path starts this way, so every link will be homepage. To avoid this it can either be moved to the bottom of route check queue to have the least priority or treated with "exact" attribute to check if whole query string is equal to http:blabla/ if there is something after / the exact attribute will return false and therefor react router will not display this page 
         */}
-        <Route exact path='/'>
-          <Homepage />
-        </Route>
-
-        <Route path='/shop/hats'>
-          <HatsPage />
-        </Route>
+        <Route path='/' component={Homepage} exact />
+        <Route path='/shop' component={ShopPage} />
 
       </Switch>
     </BrowserRouter>
