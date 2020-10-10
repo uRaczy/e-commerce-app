5ffe7e14c293c0415f8474f0d2fa03c08c0f8ad6
uRaczy <raczy7@gmail.com>
2020-10-11 01:36:11 +0200

    Added signup component with input formula and button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
 
 function App() {
   return(
     <BrowserRouter>
+    <Header />
       <Switch>
         {/* 
         for router without "exact" attribute every route is basically a "/" path, because every path starts this way, so every link will be homepage. To avoid this it can either be moved to the bottom of route check queue to have the least priority or treated with "exact" attribute to check if whole query string is equal to http:blabla/ if there is something after / the exact attribute will return false and therefor react router will not display this page 
         */}
         <Route path='/' component={Homepage} exact />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
 
       </Switch>
     </BrowserRouter>
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+import './sign-in.style.scss';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component'
+
+class SignIn extends Component {
+  constructor (props) {
+    super();
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ email: '', password: '' });
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [ name ]: value });
+  }
+
+  render() {
+    return (
+      <div className='sign-in'>
+        <h1>I already have an account</h1>
+        <h2>Sign in with your email and password</h2>
+
+        <form onSubmit={this.handleSubmit}>
+          
+          <FormInput 
+            name='email' 
+            value={this.state.email} 
+            onChange={this.handleChange}
+            label='email'
+            required 
+          />
+
+          <FormInput 
+            name='password' 
+            value={this.state.password} 
+            onChange={this.handleChange}
+            label='password'
+            required 
+          />
+
+          <CustomButton type="submit"> 
+            Sign In 
+          </CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
