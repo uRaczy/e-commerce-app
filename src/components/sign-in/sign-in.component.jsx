2a049195e397f2cd13791427548224d6a235cf36
uRaczy <raczy7@gmail.com>
2020-11-14 18:01:18 +0100

    Updated sign in with email and password to work with firestore

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,68 +1,79 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './sign-in.style.scss';
+import "./sign-in.style.scss";
 
-import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utility';
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utility";
 
 class SignIn extends Component {
-  constructor (props) {
+  constructor(props) {
     super();
 
     this.state = {
-      email: '',
-      password: ''
-    }
-
+      email: "",
+      password: "",
+    };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
-  }
+    const { email, password } = this.state;
 
-  handleChange = event => {
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      if (errorCode === "auth/wrong-password") {
+        alert("Wrong password.");
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+    }
+
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (event) => {
     const { name, value } = event.target;
-    this.setState({ [ name ]: value });
-  }
+    this.setState({ [name]: value });
+  };
 
   render() {
     return (
-      <div className='sign-in'>
+      <div className="sign-in">
         <h1>I already have an account</h1>
         <h2>Sign in with your email and password</h2>
 
         <form onSubmit={this.handleSubmit}>
-          
-          <FormInput 
-            name='email' 
-            value={this.state.email} 
+          <FormInput
+            name="email"
+            value={this.state.email}
             onChange={this.handleChange}
-            label='email'
-            required 
-          />
-
-          <FormInput 
-            name='password'
-            type='password'
-            value={this.state.password} 
-            onChange={this.handleChange}
-            label='password'
+            label="email"
             required
           />
 
-          <CustomButton type="submit"> 
-            Sign In 
-          </CustomButton>
+          <FormInput
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            label="password"
+            required
+          />
+
+          <CustomButton type="submit" onClick={this.handleSubmit}>Sign In</CustomButton>
           <CustomButton onClick={signInWithGoogle}>
             Sign In With Google
           </CustomButton>
           {/* <CustomButton onClick={signInWithFacebook}>
             Sign In With Facebook
           </CustomButton> */}
-          
         </form>
       </div>
     );
